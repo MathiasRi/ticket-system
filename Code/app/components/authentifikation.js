259710ac7c3ac74009f61a01633a636459a340d2
evermarr <benediktgav@gmail.com>
2019-03-04 17:34:47 +0100

    [DEV] Login Screen erweitert

---
+++ Code/app/components/authentifikation.js
@@ -0,0 +1,14 @@
+"use strict";
+
+app.component("authentifikation", {
+    templateUrl: "components/authentifikation.html",
+    controller: "AuthentifikationController",
+    bindings: {}
+});
+
+
+app.controller("AuthentifikationController", function ($log) {
+
+    $log.debug("AuthentifikationController()");
+
+});
