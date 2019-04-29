c6f53bda535809369daf1363b5e5560a118093dd
Florian Ploiner <florian.ploiner@htl.rennweg.at>
2019-04-29 16:54:49 +0200

    [DEV] Profilstate bis auf AJAX mehr oder weniger in Ordnung

--- Code/app/components/profil.js
+++ Code/app/components/profil.js
@@ -17,50 +17,19 @@ app.config(function ($stateProvider) {
 });
 
 
-app.controller("ProfilController", function ($log) {
+app.controller("ProfilController", function ($log, $http) {
 
     $log.debug("ProfilController()");
 
-    this.edit_name_bool = true;
-    this.hide_names_save_button_bool = true;
+    this.profilDaten = () => {
+        $http.get('localhost/?get=userdata&email=')
+    }
 
-    this.editNames = () => {
-        this.edit_name_bool = false;
-        this.hide_vorname_bool = true;
-        this.hide_nachname_bool = true;
-        this.hide_names_edit_button_bool = true;
-        this.hide_names_save_button_bool = false;
-    };
-
-    this.saveNames = () => {
-
-    };
-
-    this.edit_email_bool = true;
-    this.hide_email_save_button_bool = true;
-
-    this.editEmail = () => {
-        this.edit_email_bool = false;
-        this.hide_email_bool = true;
-        this.hide_email_edit_button_bool = true;
-        this.hide_email_save_button_bool = false;
-    };
-
-    this.saveEmail = () => {
-
-    };
-
-    this.edit_passwort_bool = true;
-    this.hide_save_passwort_div = true;
-
-    this.editPasswort = () => {
-        this.edit_passwort_bool = false;
-        this.hide_edit_passwort_div = true;
-        this.hide_save_passwort_div = false;
-    };
-
-    this.savePasswort = () => {
-
-    };
-
+    this.unsavedChanges = () => {
+        if (this.profil_vorname !== thisprofilDaten['vorname'] || this.profil_nachname !== thisprofilDaten['nachname'] || this.profil_email !== thisprofilDaten['email'] || (this.profil_passwort !== thisprofilDaten['passwort'] && this.profil_passwort !== this.profil_passwort_wiederholen)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 });
