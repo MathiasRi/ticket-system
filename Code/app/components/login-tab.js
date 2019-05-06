6512863d9b9394e89bcfa6e13be9b367d807dbcf
Florian Ploiner <florian.ploiner@htl.rennweg.at>
2019-05-06 17:17:35 +0200

    [DEV] Durchbruch Pt.4

--- Code/app/components/login-tab.js
+++ Code/app/components/login-tab.js
@@ -7,13 +7,13 @@ app.component("loginTab", {
 });
 
 
-app.controller("LoginTabController", function ($log, $http, $state) {
+app.controller("LoginTabController", function ($log, $http, $state, StorageService, User) {
 
     $log.debug("LoginTabController()");
 
     this.loginUser = () => {
         let login_promise = $http
-            .get(`http://localhost/ticket-system/Code/app/index.php`,
+            .get(`http://localhost/GAV/ticket-system/Code/app/index.php`,
                 {
                     params : {
                         get : 'userdata',
@@ -24,6 +24,17 @@ app.controller("LoginTabController", function ($log, $http, $state) {
             .then(response => {
                 console.log(response);
                 if(response.data.length != 0) {
+
+                    this.pk_individual_id = response.data[0]['PK_Individual_Id'];
+                    this.email = response.data[0]['Email'];
+                    this.password = response.data[0]['Password'];
+                    this.firstname = response.data[0]['FirstName'];
+                    this.lastname = response.data[0]['LastName'];
+                    this.fk_token = response.data[0]['FK_Token'];
+
+                    this.user = new User(this.pk_individual_id, this.email, this.password, this.firstname, this.lastname, this.fk_token);
+                    StorageService.speichern(this.user);
+
                     $state.go('ticket-ansicht');
                 } else {
                     alert('Failed to log in.');
