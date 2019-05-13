d698871f83e99ba85f089c3d67491b60d0586a4b
andrewchung1 <andrew240801@gmail.com>
2019-05-13 14:49:19 +0200

    [DEV] alles in index.html eingebunden

--- Code/app/components/login-tab.js
+++ Code/app/components/login-tab.js
@@ -13,33 +13,33 @@ app.controller("LoginTabController", function ($log, $http, $state, StorageServi
 
     this.loginUser = () => {
         let login_promise = $http
-            .get(`http://localhost/GAV/ticket-system/Code/app/index.php`,
-                {
-                    params : {
-                        get : 'userdata',
-                        email : this.login_email,
-                        pw : this.login_password
-                    }
-                })
-            .then(response => {
-                console.log(response);
-                if(response.data.length != 0) {
+            .get('http://localhost/ticket-system/Code/app/index.php',
+        {
+            params : {
+                get : 'userdata',
+                    email : this.login_email,
+                    pw : this.login_password
+            }
+        })
+    .then(response => {
+            console.log(response);
+            if(response.data.length != 0) {
 
-                    this.pk_individual_id = response.data[0]['PK_Individual_Id'];
-                    this.email = response.data[0]['Email'];
-                    this.password = response.data[0]['Password'];
-                    this.firstname = response.data[0]['FirstName'];
-                    this.lastname = response.data[0]['LastName'];
-                    this.fk_token = response.data[0]['FK_Token'];
+                this.pk_individual_id = response.data[0]['PK_Individual_Id'];
+                this.email = response.data[0]['Email'];
+                this.password = response.data[0]['Password'];
+                this.firstname = response.data[0]['FirstName'];
+                this.lastname = response.data[0]['LastName'];
+                this.fk_token = response.data[0]['FK_Token'];
 
-                    this.user = new User(this.pk_individual_id, this.email, this.password, this.firstname, this.lastname, this.fk_token);
-                    StorageService.speichern(this.user);
+                this.user = new User(this.pk_individual_id, this.email, this.password, this.firstname, this.lastname, this.fk_token);
+                StorageService.speichern(this.user);
 
-                    $state.go('ticket-ansicht');
-                } else {
-                    alert('Failed to log in.');
-                }
-            })
+                $state.go('ticket-ansicht');
+            } else {
+                alert('Failed to log in.');
+            }
+        })
             .catch(response => {
                 $log.error('Error!');
             });
