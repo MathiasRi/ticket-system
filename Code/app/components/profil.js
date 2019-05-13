ab1ffbf0fdaf360a23f2b1c091b52fb8056a1ae0
Florian Ploiner <florian.ploiner@htl.rennweg.at>
2019-05-13 17:42:14 +0200

    [DEV] Profil

--- Code/app/components/profil.js
+++ Code/app/components/profil.js
@@ -23,21 +23,21 @@ app.controller("ProfilController", function ($log, $http, StorageService, User) 
     $log.debug("ProfilController()");
 
 
-        this.profilDaten = StorageService.laden();
+    this.profilDaten = StorageService.laden();
 
-        this.datenChange = () => {
-            $http.get(`http://localhost/ticket-system/Code/app/index.php`,
-                {
-                    params: {
-                        update: 'userdata',
-                        vorname: this.profil_vorname,
-                        nachname: this.profil_nachname,
-                        email: this.profil_email,
-                        pw: this.profil_password,
-                        token: this.profilDaten.fk_token
-                    }
-                });
-        }
+    this.datenChange = () => {
+        $http.get(`http://192.168.43.135/ticket-system/Code/app/index.php`,
+            {
+                params: {
+                    update: 'userdata',
+                    vorname: this.profil_vorname,
+                    nachname: this.profil_nachname,
+                    email: this.profil_email,
+                    pw: this.profil_password,
+                    token: this.profilDaten.fk_token
+                }
+            });
+    }
 
     this.unsavedChanges = () => {
         if (this.profil_vorname !== thisprofilDaten['vorname'] || this.profil_nachname !== thisprofilDaten['nachname'] || this.profil_email !== thisprofilDaten['email'] || (this.profil_passwort !== thisprofilDaten['passwort'] && this.profil_passwort !== this.profil_passwort_wiederholen)) {
